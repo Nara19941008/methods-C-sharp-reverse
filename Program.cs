e58ae6e94d794774a583e9ee74089424c7a578a0
Nara19941008 <narminim@code.edu.az>
2022-06-22 00:43:31 +0400

    Added bio table

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿//using System;
+
+//namespace methods_Csharp
+//{
+//    class StringHelper
+//    {
+//        public static string ReverseString(string myStr)
+//        {
+//            char[] myArr = myStr.ToCharArray();
+//            Array.Reverse(myArr);
+//            return new string(myArr);
+//        }
+//    }
+
+//    class myProgram
+//    {
+//        static void Main()
+//        {
+//            Console.WriteLine(StringHelper.ReverseString("Narmin"));
+
+//        }
+//    }
+//}
